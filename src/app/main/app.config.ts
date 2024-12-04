bff5ad3b945307a83c345245f3745044ea86561a
kenny5033 <dazrerx@gmail.com>
2024-12-04 12:53:28 -0500

    user service

--- src/app/main/app.config.ts
+++ src/app/main/app.config.ts
@@ -4,6 +4,15 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7MUgDCfOHwxFY21DQZ7bRioLNTcsyeZU",
+  authDomain: "chatapp-c40e9.firebaseapp.com",
+  projectId: "chatapp-c40e9",
+  storageBucket: "chatapp-c40e9.firebasestorage.app",
+  messagingSenderId: "411258806048",
+  appId: "1:411258806048:web:aa6a5142807f3690f04145"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
