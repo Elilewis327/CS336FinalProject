7cc09f0975114c0a23d4e894cac947b6348fe7dc
kenny5033 <dazrerx@gmail.com>
2024-12-11 23:38:31 -0500

    slightly better styles?

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, output } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { DbService } from "../db-service/db-service.service";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgbModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
@@ -14,6 +15,8 @@ export class SettingsComponent {
   public username = this.dbService.user!.username;
   public fontSize: number = localStorage["fontSize"] || 0;
   public fontSizeOut = output<number>();
+
+  public sideMenuCollapsed = true;
 
   public updateFontSize(e: Event): void {
     this.fontSizeOut.emit((e.target! as HTMLFormElement)["valueAsNumber"]);
